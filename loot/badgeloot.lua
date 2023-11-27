0607aadd62a98cdda5763c31f85709e1e92ceb80
James Huston <james@jameshuston.net>
2023-11-27 17:48:48 -0500

    Got script working to double up on badges of justice

--- loot/badgeloot.lua
+++ loot/badgeloot.lua
@@ -1,2 +1,13 @@
-local PLAYER_EVENT_ON_LOOT_ITEM = 32
 
+local PLAYER_EVENT_ON_LOOT_ITEM = 32
+local ITEM_BADGE_OF_JUSTICE = 29434
+
+local function OnPlayerLootItem(event, player, item, count)
+    local entry = item:GetDisplayId()
+
+    if (entry == 29434) then
+        player:AddItem(ITEM_BADGE_OF_JUSTICE, count)
+    end
+end
+
+RegisterPlayerEvent(PLAYER_EVENT_ON_LOOT_ITEM, OnPlayerLootItem)
