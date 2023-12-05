ab5c05e41b367cdf2c3db3f8ae3502013a38fcbf
James Huston <james@jameshuston.net>
2023-12-05 09:14:30 -0500

    Initial working update-rep-on-login script

--- reputation/sharedrep.lua
+++ reputation/sharedrep.lua
@@ -1,23 +1,54 @@
 
 local PLAYER_EVENT_ON_LOGIN = 3
+local racesHorde = "(2, 5, 6, 8, 10)"
+local racesAlliance = "(1, 3, 4, 7, 11)"
+
+local function interp(s, tab)
+    return (s:gsub('($%b{})', function(w) return tab[w:sub(3, -2)] or w end))
+end
 
 local function OnPlayerLogin(event, player)
     local accountId = player:GetAccountId()
+    local guid = player:GetGUID()
+    player:Say("My Guid is ".. tostring(guid), 0)
 
-    local raceQuery = "(2, 5, 6, 8, 10)"
+    local raceQuery = racesHorde
     if player:IsAlliance() then
-        raceQuery = "(1, 3, 4, 7, 11)"
+        raceQuery = racesAlliance
     end
 
-    results = CharDBQuery("SELECT guid FROM characters WHERE account = " 
-        .. accountId
-        .. " AND race IN ".. raceQuery
-    )
+    local queryString = [[
+        SELECT
+            guid 
+            , faction
+            , MAX(standing)
+        FROM acore_characters.character_reputation
+        WHERE standing != 0
+        AND GUID IN (
+            SELECT
+                guid
+            FROM characters
+            WHERE
+                account = ${accountParam}
+                AND guid != ${guidParam}
+                AND race IN ${raceParam}
+        )
+        GROUP BY faction;
+    ]]
 
-    local playerList = {}
+    local results = CharDBQuery(interp(queryString, 
+        { accountParam = accountId, raceParam = raceQuery, guidParam = tostring(guid) }))
+
     if results then
         repeat
-            playerList[#playerList + 1] = results:GetUInt32(0)
+            local repGuid = results:GetUInt32(0)
+            local faction = results:GetUInt32(1)
+            local standing = results:GetUInt32(2)
+            -- player:Say("GUID " .. repGuid .. " Faction ".. tostring(faction) .. " standing ".. tostring(standing), 0)
+
+            if repGuid ~= guid then
+                player:SetReputation(faction, standing)
+            end
         until not results:NextRow()
     end
 
